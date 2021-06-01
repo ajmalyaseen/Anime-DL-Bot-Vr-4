d30f8477adbc6a9975aa067fab718d1e3be72773
Diago <83084169+ajmalyaseen@users.noreply.github.com>
2021-06-01 20:32:07 +0530

    Update back.py

--- src/back.py
+++ src/back.py
@@ -15,6 +15,11 @@ def back(client, callback_query):
         [InlineKeyboardButton("Search Anime Inline", switch_inline_query_current_chat="")]
     ]
     reply_markup = InlineKeyboardMarkup(keyb)
-    query.edit_message_caption(caption="""start_message
+    query.edit_message_caption(caption="""
+Hi {}!,
+
+𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐜𝐚𝐧 𝐆𝐞𝐭 𝐲𝐨𝐮𝐫 𝐟𝐚𝐯𝐨𝐮𝐫𝐢𝐭𝐞 𝐀𝐧𝐢𝐦𝐞 𝐚𝐧𝐝 𝐈𝐭 𝐩𝐫𝐨𝐯𝐢𝐝𝐞𝐬 𝐅𝐑𝐄𝐄 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐋𝐢𝐧𝐤 𝐰𝐢𝐭𝐡 𝐚 𝐟𝐚𝐬𝐭𝐞𝐬𝐭 𝐬𝐞𝐫𝐯𝐞𝐫(𝐆𝐨𝐨𝐠𝐥𝐞 𝐝𝐫𝐢𝐯𝐞)...
+
+ ©ᴄᴏᴅᴇʀᴢʜᴇx
 
 """, parse_mode="markdown", reply_markup=reply_markup)
