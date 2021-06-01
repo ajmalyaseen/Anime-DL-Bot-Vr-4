2b07f2b3c16b0a2da4e16e0e9418277d32c9156f
Diago <83084169+ajmalyaseen@users.noreply.github.com>
2021-06-01 09:57:21 +0530

    Update start_message.py

--- src/start_message.py
+++ src/start_message.py
@@ -10,14 +10,16 @@ from pyrogram.types import *
 
 def start_message(client, message):
     kkeeyyb = [
-        [InlineKeyboardButton("Instructions", callback_data="instructions")],
+        [[
+            InlineKeyboardButton("📫UPDATES", url="https://t.me/CoderzHEX"),
+            InlineKeyboardButton("💡HELP", callback_data= "instructions")
+            ],[
+            InlineKeyboardButton("📕ABOUT", callback_data= "about"),
+            InlineKeyboardButton("🔐 CLOSE", callback_data= "close")
+            ]]
     ]
     reply_markup = InlineKeyboardMarkup(kkeeyyb)
     pic_url = "https://telegra.ph/file/dac37677227331b5a29f3.jpg"
     message.reply_photo(pic_url, caption=f"""**Hi {message.chat.first_name}**,
 
-𝓦𝓮𝓵𝓬𝓸𝓶𝓮 𝓽𝓸 𝓣𝓮𝓵𝓮𝓰𝓻𝓪𝓶 𝓐𝓷𝓲𝓶𝓮 𝓢𝓽𝓮𝓻𝓪𝓶 𝓫𝓸𝓽, 𝓗𝓮𝓻𝓮 𝔂𝓸𝓾 𝓬𝓪𝓷 𝓓𝓸𝔀𝓷𝓵𝓸𝓪𝓭 𝓪𝓵𝓵 𝓐𝓷𝓲𝓶𝓮 𝓯𝓸𝓻 𝓕𝓡𝓔𝓔 😁
-
-!!!  __𝓟𝓵𝓮𝓪𝓼𝓮 𝓻𝓮𝓪𝓭 𝓪𝓵𝓵 𝓽𝓱𝓮 𝓲𝓷𝓼𝓽𝓻𝓾𝓬𝓽𝓲𝓸𝓷𝓼 𝓪𝓫𝓸𝓾𝓽 𝓽𝓱𝓮 𝓫𝓸𝓽 𝓫𝓮𝓯𝓸𝓻𝓮 𝓼𝓾𝓻𝓯𝓲𝓷𝓰 𝓸𝓷...__  
-
-𝓢𝓮𝓮 /whats_new 𝓽𝓸 𝓴𝓷𝓸𝔀 𝓪𝓫𝓸𝓾𝓽 𝓵𝓪𝓽𝓮𝓼𝓽 𝓾𝓹𝓭𝓪𝓽𝓮𝓼...""", reply_markup=reply_markup, parse_mode="markdown")
+𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐜𝐚𝐧 𝐆𝐞𝐭 𝐲𝐨𝐮𝐫 𝐟𝐚𝐯𝐨𝐮𝐫𝐢𝐭𝐞 𝐀𝐧𝐢𝐦𝐞 𝐚𝐧𝐝 𝐈𝐭 𝐩𝐫𝐨𝐯𝐢𝐝𝐞𝐬 𝐅𝐑𝐄𝐄 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐋𝐢𝐧𝐤 𝐰𝐢𝐭𝐡 𝐚 𝐟𝐚𝐬𝐭𝐞𝐬𝐭 𝐬𝐞𝐫𝐯𝐞𝐫(𝐆𝐨𝐨𝐠𝐥𝐞 𝐝𝐫𝐢𝐯𝐞)
