94dc582c48a97c631905b279e33b9629ab340a89
Diago <83084169+ajmalyaseen@users.noreply.github.com>
2021-06-03 08:15:42 +0530

    Update start_message.py

--- src/start_message.py
+++ src/start_message.py
@@ -17,4 +17,4 @@ def start_message(client, message):
     message.reply_text(f"""**Hey👋 {message.chat.first_name}**,
 𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐜𝐚𝐧 𝐆𝐞𝐭 𝐲𝐨𝐮𝐫 𝐟𝐚𝐯𝐨𝐮𝐫𝐢𝐭𝐞 𝐀𝐧𝐢𝐦𝐞 𝐚𝐧𝐝 𝐈𝐭 𝐩𝐫𝐨𝐯𝐢𝐝𝐞𝐬 𝐅𝐑𝐄𝐄 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐋𝐢𝐧𝐤 𝐰𝐢𝐭𝐡 𝐚 𝐟𝐚𝐬𝐭𝐞𝐬𝐭 𝐬𝐞𝐫𝐯𝐞𝐫(𝐆𝐨𝐨𝐠𝐥𝐞 𝐝𝐫𝐢𝐯𝐞)
 
-NOTE :- PRESS THE LAST LARGE BUTTON NAMED HELP TO SEE HOW THIS BOT WORKS OR JUST SEND""", reply_markup=reply_markup, parse_mode="markdown")
+NOTE :- PRESS THE  BUTTON NAMED HELP TO SEE HOW THIS BOT WORKS""", reply_markup=reply_markup, parse_mode="markdown")
