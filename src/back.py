baa646a85fc3dcd2de1ce2d8e5d0a6151150d9c7
Diago <83084169+ajmalyaseen@users.noreply.github.com>
2021-06-03 08:27:53 +0530

    Update back.py

--- src/back.py
+++ src/back.py
@@ -14,8 +14,8 @@ def back(client, callback_query):
         [InlineKeyboardButton("♻️HELP", callback_data="instructions"),InlineKeyboardButton("📕ABOUT", callback_data="about")],
     ]
     reply_markup = InlineKeyboardMarkup(kkeeyyb)
-    query.edit_reply_text(f"""**Hey👋 {message.chat.first_name}**,
+    query.edit_message_text(f"""**Hey👋**,
 
 𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐜𝐚𝐧 𝐆𝐞𝐭 𝐲𝐨𝐮𝐫 𝐟𝐚𝐯𝐨𝐮𝐫𝐢𝐭𝐞 𝐀𝐧𝐢𝐦𝐞 𝐚𝐧𝐝 𝐈𝐭 𝐩𝐫𝐨𝐯𝐢𝐝𝐞𝐬 𝐅𝐑𝐄𝐄 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐋𝐢𝐧𝐤 𝐰𝐢𝐭𝐡 𝐚 𝐟𝐚𝐬𝐭𝐞𝐬𝐭 𝐬𝐞𝐫𝐯𝐞𝐫(𝐆𝐨𝐨𝐠𝐥𝐞 𝐝𝐫𝐢𝐯𝐞)
 
-NOTE :- PRESS THE  BUTTON NAMED HELP TO SEE HOW THIS BOT WORKS""", reply_markup=reply_markup, parse_mode="markdown")
+NOTE :- CLICK THE HELP BUTTON TO KNOW MORE""", reply_markup=reply_markup, parse_mode="markdown")
